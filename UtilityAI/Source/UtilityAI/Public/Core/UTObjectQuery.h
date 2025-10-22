a47cc3efffcf0a5edbd8247f290c4de4dd37fcd8
Alex Jobe <ajobe15@gmail.com>
2025-10-22 13:53:39 -0500

    Object query fix

--- UtilityAI/Source/UtilityAI/Public/Core/UTObjectQuery.h
+++ UtilityAI/Source/UtilityAI/Public/Core/UTObjectQuery.h
@@ -10,29 +10,55 @@ struct UTObjectQuery
 {
 	std::unordered_set<std::string> RequiredTags;
 	std::unordered_set<std::string> ExcludedTags;
+	std::unordered_set<std::string> AnyTags;
 
-	std::function<bool(const T& Object)> PredicateFn; // Optional custom filter
+	std::function<bool(const T&)> PredicateFn; // Optional custom filter
 
 	bool Matches(const T& Object) const
 	{
+		// Must have all required tags
 		for (const auto& Tag : RequiredTags)
 		{
-			if (!T.Tags.contains(Tag))
+			if (!Object.Tags.contains(Tag))
 			{
 				return false;
 			}
 		}
+
+		// Must NOT have excluded tags
 		for (const auto& Tag : ExcludedTags)
 		{
-			if (T.Tags.contains(Tag))
+			if (Object.Tags.contains(Tag))
 			{
 				return false;
 			}
 		}
-		if (PredicateFn && !PredicateFn(T))
+
+		// Must have at least one of the "Any" tags (if any are specified)
+		if (!AnyTags.empty())
+		{
+			bool bHasAny = false;
+			for (const auto& Tag : AnyTags)
+			{
+				if (Object.Tags.contains(Tag))
+				{
+					bHasAny = true;
+					break;
+				}
+			}
+			if (!bHasAny)
+			{
+				return false;
+			}
+		}
+
+		// Optional predicate
+		if (PredicateFn && !PredicateFn(Object))
 		{
 			return false;
 		}
+
+		return true;
 	}
 };
 }
